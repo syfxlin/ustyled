25af9fdc56eb1661979d51814df9002db1417b0b
syfxlin <i@ixk.me>
2022-06-14 00:10:34 +0800

    build: Version 2.1.1

--- src/hooks/use-responsive.ts
+++ src/hooks/use-responsive.ts
@@ -4,15 +4,15 @@ import { Breakpoints } from "../types";
 
 export const useUp = (minWidth: keyof Breakpoints) => {
   const { ctx } = useU();
-  return useMediaQuery(`(min-width: ${ctx.breakpoints[minWidth]}px)`);
+  return useMediaQuery(`(min-width: ${ctx.breakpoints[minWidth]})`);
 };
 
 export const useDown = (maxWidth: keyof Breakpoints) => {
   const { ctx } = useU();
-  return useMediaQuery(`(min-width: ${ctx.breakpoints[maxWidth]}px)`);
+  return useMediaQuery(`(min-width: ${ctx.breakpoints[maxWidth]})`);
 };
 
 export const useBetween = (minWidth: keyof Breakpoints, maxWidth: keyof Breakpoints) => {
   const { ctx } = useU();
-  return useMediaQuery(`(min-width: ${ctx.breakpoints[minWidth]}px) and (max-width: ${ctx.breakpoints[maxWidth]}px)`);
+  return useMediaQuery(`(min-width: ${ctx.breakpoints[minWidth]}) and (max-width: ${ctx.breakpoints[maxWidth]})`);
 };
