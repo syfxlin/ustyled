5c5d12cfa5fbdd43823f7c11974f20625ccef618
syfxlin <i@ixk.me>
2022-06-26 20:04:08 +0800

    build: Version 2.1.3

--- src/hooks/use-u.ts
+++ src/hooks/use-u.ts
@@ -5,8 +5,8 @@ import { createCss } from "../css";
 export const useU = () => {
   const { ctx, mode, setMode } = useUstyled();
 
-  const api = useMemo(() => ctx.api.map((fn) => fn(ctx)).reduce(Object.assign, {}), [ctx, ctx.api]);
-  const css = useMemo(() => createCss(api), [ctx, api]);
+  const api = useMemo(() => ctx.api.map((fn) => fn(ctx)).reduce(Object.assign, {}), [ctx, ctx.api, mode, ctx.mode]);
+  const css = useMemo(() => createCss(api), [ctx, api, mode, ctx.mode]);
 
   return {
     ctx,
